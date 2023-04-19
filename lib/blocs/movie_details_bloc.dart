375e183ba8709b78767fdff3e01c132eaf5e03a9
Shine Aung Khant <shineaungkhant1@gmail.com>
2023-04-19 21:51:48 +0630

    Initial commit

---
+++ lib/blocs/movie_details_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:movie_app/data/models/movie_model_impl.dart';
+import 'package:movie_app/data/vos/actor_vo.dart';
+
+import '../data/models/movie_model.dart';
+import '../data/vos/movie_vo.dart';
+
+class MovieDetailsBLoc extends ChangeNotifier{
+  /// Stream Controllers
+  late MovieVO mMovie;
+  late List<ActorVO> mCastList;
+  late List<ActorVO> mCrewList;
+
+
+  /// Models
+   MovieModel mMovieModel = MovieModelImpl();
+
+   MovieDetailsBLoc(int movieId){
+     /// Movie Details
+     mMovieModel.getMovieDetails(movieId).then((movie) {
+      mMovie=movie;
+      notifyListeners();
+     });
+
+     /// Movie Details From Database
+     mMovieModel.getMovieDetailsFromFromDatabase(movieId).then((movie){
+       mMovie=movie;
+       notifyListeners();
+     });
+
+     mMovieModel.getCreditsByMovie(movieId).then((castAndCrew){
+     mCastList=castAndCrew.first??[];
+     mCrewList=castAndCrew.first??[];
+     });
+
+   }
+
+
+
+}
